947873d364f7d5d2746ad9d3b26a0f521caa1d5d
AmeliaHuijun <33433816+AmeliaHuijun@users.noreply.github.com>
2019-05-02 17:50:01 -0500

    Update Maximum Subarray
    The previous maxSub3 is WRONG, I need to consider the following situation:
          input = [1,2,4,-1,-20,5,-1,4];
          M[i] =   [1,3,7, 6,-14,5, 4,8];
                                                 I
    	//         S1              S2
    	//               E1               E2
    	The excepted result is start=5, end=7, gmax=8; but the execution result is "Start: 0, END: 7, gmax: 8."
    	So I need reconsider. I need two set of S&E. whenever there is a new global max, I will update the solution start and solution end.
    	//One set: start;end
    	//Two set: solu_start; solu_end
    	When to update stat-->when M[i-1]<0
            When to update solu_end --> when global max changed
    	When to update solu_start-->when solu_end >= start

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -16,8 +16,15 @@ public class MaximumSubarray {
 		System.out.println(maxSub(B));
 		System.out.println(maxSub2(B));
 		
+		
 		maxSub3(A);
 		maxSub3(B);
+		
+		int[] C = {1,2,4,-1,-20,5,-1,4};
+		//M[i] =  [1,3,7, 6,-14,5, 4,8];
+		maxSub3(C);
+		maxSub4(C);
+		//the excepted result is start=5, end=7, gmax=8; but the execution result is "Start: 0, END: 7, gmax: 8."
 	}
 	//eg1: input = [1,2,4,-1,-2,10,-1];
 	//      M[i] = [1,3,7,6,  4,14, 13];
@@ -107,5 +114,49 @@ public class MaximumSubarray {
 		System.out.print("Start: "+start+", END: "+end+", gmax: "+gmax+".  ");
 	}
 	
+	//the previous maxSub3 is WRONG, we need to consider the following situation
+	////input = [1,2,4,-1,-20,5,-1,4];
+    //M[i] =    [1,3,7, 6,-14,5, 4,8];
+    //                      I
+	//           S1           S2
+	//               E1            E2
+	//the excepted result is start=5, end=7, gmax=8; but the execution result is "Start: 0, END: 7, gmax: 8."
+	//So I need reconsider. I need two set of S&E. whenever there is a new global max, I will update the solution start and solution end.
+	//start;end
+	//solu_start; solu_end
+	//when to update stat-->when M[i-1]<0
+	//when to update solu_start-->when M[i-1]<0 & when global max changed
+	//when to update solu_end --> when solu_start changed
+	public static void maxSub4(int[] input) {
+		//Base Case
+		int n = input.length;
+		int[] M = new int[n+1];
+		M[0] = input[0];
+		int gmax = M[0];
+		
+		int start = 0;
+		
+		int solu_start = 0;
+		int solu_end = 0;
+		//Induction Rule
+		for(int i = 1; i < n; i++) {
+			if(M[i-1] >= 0) {
+				M[i] = M[i-1] + input[i];
+				if(M[i]>gmax) {
+					solu_end = i;
+				}
+			}
+			else {
+				M[i] = input[i];
+				start = i;
+			}
+			gmax = Math.max(gmax, M[i]);
+			if(solu_end>=start) {
+				solu_start = start;
+			}
+		}
+		
+		System.out.print("Start: "+solu_start+", END: "+solu_end+", gmax: "+gmax+".  ");
+	}
 
 }
